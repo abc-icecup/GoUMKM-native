65b01bff425b96e5f7bc606c7314bc06e9271339
Ratna Ruspita Dewi <ratnadhewi6@gmail.com>
2025-06-13 23:00:49 +0700

    pengerjaan edit profil

--- pages/tambah_produk.php
+++ pages/tambah_produk.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GoUMKM - Formulir Profil Usaha</title>
+    <title>GoUMKM - Tambahkan Produk Anda</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
    <link rel="stylesheet" href="../css/tambah_produk.css">
